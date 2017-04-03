a0f0ab9154469ea5dad681a690df8967b3a0d8fd
jburre <Jan.H.Burre@gmail.com>
2017-04-03 11:47:32 +0200

    feat: added a login site
    created a login route and a pseudo-random state for every session,
    which is displayed for learning and debug reasons

--- vagrant/project.py
+++ vagrant/project.py
@@ -5,10 +5,24 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Restaurant, MenuItem, Base
 
+# new steps for Auth
+# since session is already used, use new name
+from flask import session as login_session
+import random, string 
+
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind=engine
 DBSession=sessionmaker(bind=engine)
 session=DBSession()
+
+
+#new route for login
+#create state token
+@app.route("/login")
+def showLogin():
+    state=''.join(random.choice(string.ascii_uppercase+string.digits) for x in xrange(32))
+    login_session['state']=state
+    return "The current session state is %s" %login_session['state']
 
 #making an API endpoint (just GET for now)
 @app.route('/restaurants/<int:restaurant_id>/menu/JSON')
