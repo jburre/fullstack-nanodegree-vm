6afc19bc7f96718a8d8b505990b8f4eea962cee2
jburre <Jan.H.Burre@gmail.com>
2017-04-03 12:24:36 +0200

    refactor: tried to alter the state
    tried to alter the state to include special characters. Maybe in
    the future. Non-ASCII code is not permitted. Added lowercase
    letters to the mix of state characters

--- vagrant/project.py
+++ vagrant/project.py
@@ -20,7 +20,7 @@ session=DBSession()
 #create state token
 @app.route("/login")
 def showLogin():
-    state=''.join(random.choice(string.ascii_uppercase+string.digits) for x in xrange(32))
+    state=''.join(random.choice(string.ascii_letters+string.digits) for x in xrange(32))
     login_session['state']=state
     return "The current session state is %s" %login_session['state']
 
